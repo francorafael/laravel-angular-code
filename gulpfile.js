a1166114a466f1f2abb8635ee7adf34916fd78db
Rafael Franco <rafael.apfsantos@gmail.com>
2016-03-17 17:50:58 -0300

    Iniciando login
    Iniciando login

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,9 @@ config.vendor_path_js = [
    config.bower_path + '/angular-messages/angular-messages.min.js',
    config.bower_path + '/angular-bootstrap/ui-bootstrap.min.js',
    config.bower_path + '/angular-strap/dist/modules/navbar.min.js',
+   config.bower_path + '/angular-cookies/angular-cookies.min.js',
+   config.bower_path + '/query-string/query-string.js',
+   config.bower_path + '/angular-oauth2/dist/angular-oauth2.min.js',
 ];
 
 config.build_path_css = config.build_path + '/css';
@@ -32,6 +35,18 @@ config.vendor_path_css = [
    config.bower_path + '/bootstrap/dist/css/bootstrap.min.css',
    config.bower_path + '/bootstrap/dist/css/bootstrap-theme.min.css',
 ];
+
+
+config.build_path_html = config.build_path + '/views';
+
+gulp.task('copy-html', function(){
+    gulp.src([
+        config.assets_path + '/js/views/**/*.html'
+    ])
+        .pipe(gulp.dest(config.build_path_html))
+        .pipe(liveReload());
+});
+
 
 gulp.task('copy-styles', function() {
    gulp.src([
@@ -62,10 +77,10 @@ gulp.task('clear-build-folder', function(){
 });
 
 gulp.task('default',['clear-build-folder'], function() {
+    gulp.start('copy-html');
     //vai ter as funcionalidades do laravel
     elixir(function(mix){
-
-        mix.styles(config.vendor_path_css.concat([config.assets_path + '/css/**/*.css']),
+                mix.styles(config.vendor_path_css.concat([config.assets_path + '/css/**/*.css']),
         'public/css/all.css',config.assets_path);
 
         mix.scripts(config.vendor_path_js.concat([config.assets_path + '/js/**/*.js']),
@@ -77,6 +92,6 @@ gulp.task('default',['clear-build-folder'], function() {
 
 gulp.task('watch-dev', ['clear-build-folder'], function(){
  liveReload.listen();
-  gulp.start('copy-styles','copy-scripts');
-  gulp.watch(config.assets_path + '/**', ['copy-styles', 'copy-scripts']);
+  gulp.start('copy-styles','copy-scripts', 'copy-html');
+  gulp.watch(config.assets_path + '/**', ['copy-styles', 'copy-scripts', 'copy-html']);
 });
---
+++ resources/assets/js/controllers/login.js
@@ -0,0 +1,13 @@
+/**
+ * Created by rafael.franco on 17/03/2016.
+ */
+angular.module('app.controllers').controller('LoginController', ['$scope', function($scope){
+    $scope.user = {
+        username: '',
+        password: ''
+    }
+
+    $scope.login = function() {
+
+    };
+}]);
